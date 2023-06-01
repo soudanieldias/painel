a7e191f61dfa5ac91723c03f575c379a00de3210
Daniel Dias <danieldias.b99@hotmail.com>
2023-05-31 23:11:03 -0300

    feat: initial adjusts

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Component, ReactNode } from "react";
+
+class Header extends Component {
+  render(): ReactNode {
+    return(
+      <div>
+        HEADER COMPONENT
+      </div>
+    )
+  }
+}
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Component, ReactNode } from "react";
+
+class Home extends Component {
+  render(): ReactNode {
+    return(
+      <div>
+        HOME COMPONENT
+      </div>
+    )
+  }
+}
+
+export default Home;
